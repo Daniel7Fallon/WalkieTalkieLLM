f0e64848692b4ebd341a312d13fc06d5a7e15259
Daniel7Fallon <daniel.fallon1@ucdconnect.ie>
2025-03-13 14:27:52 +0000

    removed whitespace, final commit of session

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,5 @@
 package org.example;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-
 public class Main {
     public static void main(String[] args) {
         if(args.length < 1) {

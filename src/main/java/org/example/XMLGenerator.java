7edafec92161603c5f4628d337b0a7b0b6e69e7d
colinW03 <colin.wang@ucdconnect.ie>
2025-03-25 20:54:52 +0000

    more work on xml generator class and clarified outline

--- src/main/java/org/example/XMLGenerator.java
+++ src/main/java/org/example/XMLGenerator.java
@@ -17,7 +17,7 @@ public class XMLGenerator {
         XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat()); // Make the XML look nice and human-readable
         try (FileWriter writer = new FileWriter("lesson.xml")) {
             xmlOutputter.output(document, writer);
-            System.out.println("Successfully generated lesson");
+            System.out.println("Successfully generated xml.");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -26,40 +26,110 @@ public class XMLGenerator {
     private Document createDocument(VignetteManager vignetteManager) {
         Element root = new Element("comic");
         Document document = new Document(root);
-
         List<VignetteSchema> vignetteSchemas = VignetteManager.getVignetteSchemas();
-        Element figures = getFigures();
 
+        root.addContent(getFigures(vignetteManager));
+        root.addContent(getScenes(vignetteSchemas));
+
+
+        // TODO
 
 
         return document;
     }
 
-    private Element getFigures() {
+    private Element getFigures(VignetteManager vignetteManager) {
+        Element figuresElement = new Element("figures");
 
         /*
-            TODO:
-            Need this code to get the information from either the vignette manager or from a new figure class
-            and input the data for two figures into the element in the correct format.
-         */
+        // Assuming VignetteManager has a method to get all figures
+        List<VignetteSchema.Figure> figures = vignetteManager.getFigures();
 
-        return new Element("figures");
+        for (VignetteSchema.Figure figure : figures) {
+            Element figureElement = new Element("figure");
+
+            figureElement.addContent(new Element("name").setText(figure.getName()));
+            figureElement.addContent(new Element("appearance").setText(figure.getAppearance()));
+            figureElement.addContent(new Element("skin").setText(figure.getSkin()));
+            figureElement.addContent(new Element("hair").setText(figure.getHair()));
+            figureElement.addContent(new Element("pose").setText(figure.getPose()));
+            figureElement.addContent(new Element("facing").setText(figure.getFacing()));
+
+            figuresElement.addContent(figureElement);
+        }
+        */
+
+        return figuresElement;
     }
 
     private Element getScenes(List<VignetteSchema> vignetteSchemas) {
         Element scenes = new Element("scenes");
 
-        // TODO
+        for (VignetteSchema vignetteSchema : vignetteSchemas) {
+            scenes.addContent(getScene(vignetteSchema));
+        }
 
         return scenes;
     }
 
+    // Scene can contain a scene and/or a rubric
     private Element getScene(VignetteSchema vignetteSchema) {
         Element scene = new Element("scene");
+
+        /*
+            Rubric Structure:
+
+            <rubric>
+                <image><\image>
+                <above><\above>
+                <below><\below>
+                <duration><\duration>
+            <\rubric>
+         */
 
         // TODO
 
         return scene;
+    }
+
+    private Element generatePanel (VignetteSchema vignetteSchema) {
+        Element panel = new Element("panel");
+
+        /*
+            Structure to generate:
+
+            <panel>
+                <above><\above>
+                <left>
+                    <figure>
+                        <id></id>
+                        <pose></pose>
+                        <facing></facing>
+                    </figure>
+                    <balloon>
+                        <content></content>
+                    </balloon>
+                </left>
+                <right>
+                    <figure>
+                        <id></id>
+                        <pose></pose>
+                        <facing></facing>
+                    </figure>
+                    <balloon>
+                        <content></content>
+                    </balloon>
+                </right>
+                <setting></setting>
+                <border></border>
+                <duration></duration>
+                <print></print>
+                <audio></audio>
+            </panel>
+
+         */
+
+        return panel;
     }
 
 

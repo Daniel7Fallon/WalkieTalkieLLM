90f3fc26e39f4dd3d3432bc277754e027fb21c89
harry <harryandlukemc@gmail.com>
2025-04-17 19:54:13 +0100

    Added more tests for utils

--- src/test/java/org/example/UtilsTest.java
+++ src/test/java/org/example/UtilsTest.java
@@ -16,4 +16,18 @@ public class UtilsTest {
         assertEquals("Aaa", StringUtil.capitalize(s2));
         assertEquals("Aaa", StringUtil.capitalize(s3));
     }
+
+    @Test
+    void testremovePluralIdentifier() {
+        assertEquals("apple", StringUtil.removePluralIdentifier("apple (Plural)"));
+        assertEquals("book", StringUtil.removePluralIdentifier("book (plural)"));
+        assertEquals("cat", StringUtil.removePluralIdentifier("cat"));
+    }
+
+    @Test
+    void testremoveSpeakerFromDialogue() {
+        assertEquals("Hello world!", StringUtil.removeSpeaker("John: Hello world!"));
+        assertEquals("How are you?", StringUtil.removeSpeaker("  Sarah :   How are you?  "));
+        assertEquals("No speaker", StringUtil.removeSpeaker("No speaker"));
+    }
 }
